774dafbb78370d05112d94cd9a039bd0808da62a
Jeff Cook <cookjj@colorado.edu>
2021-01-27 19:09:50 -0700

    more3

--- json.js
+++ json.js
@@ -1,6 +1,9 @@
 $().ready(function(){
-    $.getJSON( "/data.json", function( data ) {
-    console.log(data);
-    $("#text").html(data["text"]);
-  });
+    $.getJSON( "/data.json", function( thedata ) {
+        $("#text").html(thedata["text"]);
+    });
+
+    $.getJSON( "/data.json", function( thedata ) {
+        $("#ed0res").html("Edition 0: " + thedata["edition_0_res"]);
+    });
 });
